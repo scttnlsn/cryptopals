5f7058198c1b946da794c465fb0494abf4ecc18c
Scott Nelson <scott@scottnelson.co>
2019-09-26 11:31:24 -0400

    1-1 hex to base64 conversion

---
+++ src/encoding.rs
@@ -0,0 +1,49 @@
+pub fn hex_to_bytes(hex: &str) -> Vec<u32> {
+    let chars: Vec<u32> = hex
+        .chars()
+        .map(|c| c.to_digit(16).unwrap())
+        .collect();
+
+    chars
+        .chunks(2)
+        .map(|pair| (pair[0] << 4) | pair[1])
+        .collect()
+}
+
+pub fn bytes_to_base64(bytes: Vec<u32>) -> String {
+    let vals: Vec<u32> = bytes
+        .chunks(3)
+        .map(|bytes| (bytes[0] << 16) | (bytes[1] << 8) | bytes[2])
+        .collect();
+
+    let char_table: Vec<char> = vec![
+        (b'A'..=b'Z').collect(),
+        (b'a'..=b'z').collect(),
+        (b'0'..=b'9').collect(),
+        vec![b'+', b'/']
+    ].iter()
+        .flatten()
+        .map(|x| *x as char)
+        .collect();
+
+    let mut result: Vec<char> = Vec::new();
+
+    for val in vals {
+        let chunks = vec![
+            ((val & 0xFC0000) >> 18) as u8,
+            ((val & 0x03F000) >> 12) as u8,
+            ((val & 0x000FC0) >> 6) as u8,
+            (val & 0x00003F) as u8
+        ];
+
+        let mut chars: Vec<char> = chunks.iter().map(|val| char_table[*val as usize]).collect();
+        result.append(&mut chars);
+    }
+
+     result.iter().collect()
+}
+
+pub fn hex_to_base64(hex: &str) -> String {
+    let bytes = hex_to_bytes(hex);
+    bytes_to_base64(bytes)
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod encoding;
+
+fn main() {
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let base64 = encoding::hex_to_base64(hex);
+    println!("{}", base64);
+    assert!(base64 == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+}
