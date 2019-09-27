270718291d9e12a322cbcbbfa5428c9e63309946
Scott Nelson <scott@scottnelson.co>
2019-09-27 10:55:13 -0400

    Remove unwrap and add tests

--- src/encoding.rs
+++ src/encoding.rs
@@ -8,10 +8,19 @@ impl ByteArray {
         ByteArray { bytes: bytes }
     }
 
-    pub fn from_hex(hex: &str) -> ByteArray {
-        let chars: Vec<u32> = hex
+    pub fn from_hex(hex: &str) -> Option<ByteArray> {
+        let maybe_chars: Vec<Option<u32>> = hex
             .chars()
-            .map(|c| c.to_digit(16).unwrap()) // TODO: method should return optional
+            .map(|c| c.to_digit(16))
+            .collect();
+
+        if maybe_chars.iter().any(|x| x.is_none()) {
+            return None;
+        }
+
+        let chars: Vec<u32> = maybe_chars
+            .iter()
+            .map(|c| c.unwrap())
             .collect();
 
         let bytes = chars
@@ -19,7 +28,7 @@ impl ByteArray {
             .map(|pair| (pair[0] << 4) | pair[1])
             .collect();
 
-        ByteArray::from_bytes(bytes)
+        Some(ByteArray::from_bytes(bytes))
     }
 
     pub fn len(&self) -> usize {
@@ -31,57 +40,48 @@ impl ByteArray {
     }
 
     pub fn hex(&self) -> String {
-        let ranges: Vec<Vec<u8>> = vec![
-            (b'0'..=b'9').collect(),
-            (b'a'..=b'f').collect()
-        ];
-
-        let char_table: Vec<char> = ranges
-            .iter()
+        let char_table: Vec<u8> = vec![(b'0'..=b'9'), (b'a'..=b'f')]
+            .into_iter()
             .flatten()
-            .map(|x| *x as char)
             .collect();
 
         let mut result: Vec<char> = Vec::new();
 
         for byte in &self.bytes {
-            let upper = (*byte as u8 & 0xF0) >> 4;
-            let lower = *byte as u8 & 0x0F;
+            let upper = (((*byte as u8 & 0xF0)) >> 4) as usize;
+            let lower = (*byte as u8 & 0x0F) as usize;
 
-            result.push(char_table[upper as usize]);
-            result.push(char_table[lower as usize]);
+            result.push(char_table[upper] as char);
+            result.push(char_table[lower] as char);
         }
 
         result.iter().collect()
     }
 
     pub fn base64(&self) -> String {
-        let vals: Vec<u32> = self.bytes
-            .chunks(3)
-            .map(|bytes| (bytes[0] << 16) | (bytes[1] << 8) | bytes[2])
-            .collect();
-
-        let char_table: Vec<char> = vec![
+        let char_table: Vec<u8> = vec![
             (b'A'..=b'Z').collect(),
             (b'a'..=b'z').collect(),
             (b'0'..=b'9').collect(),
             vec![b'+', b'/']
-        ].iter()
-            .flatten()
-            .map(|x| *x as char)
+        ].into_iter().flatten().collect();
+
+        let triplets: Vec<u32> = self.bytes
+            .chunks(3)
+            .map(|bytes| (bytes[0] << 16) | (bytes[1] << 8) | bytes[2])
             .collect();
 
         let mut result: Vec<char> = Vec::new();
 
-        for val in vals {
-            let chunks = vec![
-                ((val & 0xFC0000) >> 18) as u8,
-                ((val & 0x03F000) >> 12) as u8,
-                ((val & 0x000FC0) >> 6) as u8,
-                (val & 0x00003F) as u8
+        for triple in triplets {
+            let bytes = vec![
+                ((triple & 0xFC0000) >> 18) as usize,
+                ((triple & 0x03F000) >> 12) as usize,
+                ((triple & 0x000FC0) >> 6) as usize,
+                (triple & 0x00003F) as usize
             ];
 
-            let mut chars: Vec<char> = chunks.iter().map(|val| char_table[*val as usize]).collect();
+            let mut chars: Vec<char> = bytes.into_iter().map(|val| char_table[val] as char).collect();
             result.append(&mut chars);
         }
 
@@ -96,5 +96,35 @@ impl ByteArray {
         }
 
         ByteArray { bytes: xored }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hex() {
+        let valid = "123ABC";
+        assert!(ByteArray::from_hex(valid).is_some());
+
+        let invalid = "123ABX";
+        assert!(ByteArray::from_hex(invalid).is_none());
+    }
+
+    #[test]
+    fn test_base64() {
+        let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let byte_array = ByteArray::from_hex(hex).unwrap();
+        let result = byte_array.base64();
+        assert_eq!(result, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+    }
+
+    #[test]
+    fn test_xor() {
+        let a = ByteArray::from_hex("1c0111001f010100061a024b53535009181c").unwrap();
+        let b = ByteArray::from_hex("686974207468652062756c6c277320657965").unwrap();
+        let xored = a.xor(&b).hex();
+        assert_eq!(xored, "746865206b696420646f6e277420706c6179");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,9 @@ fn find_key(cipher: &encoding::ByteArray) -> encoding::ByteArray {
 }
 
 fn main() {
-    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let byte_array = encoding::ByteArray::from_hex(hex);
-    let base64 = byte_array.base64();
-    println!("{}", base64);
-    assert!(base64 == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
 
-    let a = encoding::ByteArray::from_hex("1c0111001f010100061a024b53535009181c");
-    let b = encoding::ByteArray::from_hex("686974207468652062756c6c277320657965");
-    let xored = a.xor(&b).hex();
-    println!("{}", xored);
-    assert!(xored == "746865206b696420646f6e277420706c6179");
 
-    let cipher = encoding::ByteArray::from_hex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+    let cipher = encoding::ByteArray::from_hex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap();
     let key = find_key(&cipher);
     let decrypted = cipher.xor(&key).string();
     println!("{}", decrypted);
