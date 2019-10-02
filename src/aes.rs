742d9c8155701cd9d0b7ef6196ff14838d8b54aa
Scott Nelson <scott@scottnelson.co>
2019-10-02 13:16:14 -0400

    2-2 added AES CBC decrypt

--- src/aes.rs
+++ src/aes.rs
@@ -1,16 +1,50 @@
 use crate::encoding::{ByteArray};
+use openssl::error::{ErrorStack};
 use openssl::symm;
+use openssl::symm::{Cipher, Mode, Crypter};
 use std::str;
 
-fn decrypt(encrypted: &ByteArray, key: &str) -> Result<Vec<u8>, openssl::error::ErrorStack> {
-    let cipher = symm::Cipher::aes_128_ecb();
+fn ecb_cipher(mode: Mode, key: &[u8], data: &[u8], pad: bool) -> Result<Vec<u8>, ErrorStack> {
+    let ecb = symm::Cipher::aes_128_ecb();
 
-    symm::decrypt(
-        cipher,
-        key.as_bytes(),
-        None,
-        &encrypted.bytes
-    )
+    let mut crypter = Crypter::new(ecb, mode, key, None)?;
+    crypter.pad(pad);
+
+    let mut out = vec![0; data.len() + ecb.block_size()];
+    let count = crypter.update(data, &mut out)?;
+    let rest = crypter.finalize(&mut out[count..])?;
+
+    out.truncate(count + rest);
+    Ok(out)
+}
+
+fn decrypt_ecb(data: &ByteArray, key: &str, pad: bool) -> Result<ByteArray, ErrorStack> {
+    ecb_cipher(Mode::Decrypt, key.as_bytes(), &data.bytes, pad).and_then(|bytes| {
+        Ok(ByteArray::from_bytes(bytes))
+    })
+}
+
+fn encrypt_ecb(data: &ByteArray, key: &str, pad: bool) -> Result<ByteArray, ErrorStack> {
+    ecb_cipher(Mode::Encrypt, key.as_bytes(), &data.bytes, pad).and_then(|bytes| {
+        Ok(ByteArray::from_bytes(bytes))
+    })
+}
+
+fn decrypt_cbc(data: &ByteArray, key: &str, iv: ByteArray) -> Result<ByteArray, ErrorStack> {
+    let mut iv = iv;
+    let mut output: Vec<u8> = Vec::new();
+
+    for block in data.bytes.chunks(16).map(|bytes| ByteArray::from_bytes(bytes.to_vec())) {
+        match decrypt_ecb(&block, key, false) {
+            Ok(res) => {
+                output.append(&mut res.xor(&iv).bytes);
+                iv = block;
+            },
+            err => return err
+        }
+    }
+
+    Ok(ByteArray::from_bytes(output))
 }
 
 #[cfg(test)]
@@ -23,11 +57,19 @@ mod tests {
         let contents = fs::read_to_string("data/7.txt").unwrap();
         let encrypted = ByteArray::from_base64(&contents.replace("\n", "")).unwrap();
         let key = "YELLOW SUBMARINE";
-
-        let bytes = decrypt(&encrypted, key).unwrap();
-        let decrypted = str::from_utf8(&bytes).unwrap();
+        let decrypted = decrypt_ecb(&encrypted, key, true).unwrap().string();
 
         assert!(decrypted.contains("VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino"))
+    }
+
+    #[test]
+    fn test_encrypt_decrypt() {
+        let key = "YELLOW SUBMARINE";
+        let data = ByteArray::from_string("foobar");
+        let encrypted = encrypt_ecb(&data, key, true).unwrap();
+        let decrypted = decrypt_ecb(&encrypted, key, true).unwrap().string();
+
+        assert_eq!(decrypted, "foobar");
     }
 
     #[test]
@@ -36,11 +78,20 @@ mod tests {
 
         let aes_encrypted: Vec<ByteArray> = contents.split("\n").filter_map(|line| {
             let encrypted = ByteArray::from_hex(line).unwrap();
-            decrypt(&encrypted, "YELLOW SUBMARINE").ok()
-        }).map(|bytes| {
-            ByteArray::from_bytes(bytes)
+            decrypt_ecb(&encrypted, "YELLOW SUBMARINE", true).ok()
         }).collect();
 
         assert_eq!(aes_encrypted.len(), 1);
+    }
+
+    #[test]
+    fn test_decrypt_cbc() {
+        let contents = fs::read_to_string("data/10.txt").unwrap();
+        let encrypted = ByteArray::from_base64(&contents.replace("\n", "")).unwrap();
+        let key = "YELLOW SUBMARINE";
+        let iv = ByteArray::from_bytes(vec![0x00; 16]);
+        let decrypted = decrypt_cbc(&encrypted, key, iv).unwrap().string();
+
+        assert!(decrypted.contains("Play that funky music white boy"));
     }
 }
